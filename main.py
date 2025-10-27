f9c4fca70f08467ee68997dc035a6934f332dc2e
angkajaya <mr.wijaya.anka@gmail.com>
2025-10-27 18:05:49 +0700

    almost done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pygame import *
+
 class GameSprite(sprite.Sprite):
    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
        super().__init__()
@@ -30,6 +31,7 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
+display.set_caption("game ping pong")
 
 game = True
 finish = False
@@ -37,9 +39,9 @@ clock = time.Clock()
 FPS = 60
 
 #creating ball and paddles   
-racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
-racket2 = Player('racket.png', 520, 200, 4, 50, 150)
-ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+racket1 = Player('glasspaddle2.png', 30, 200, 4, 50, 150) 
+racket2 = Player('glasspaddle2.png', 520, 200, 4, 50, 150)
+ball = GameSprite('ball_small.png', 200, 200, 4, 50, 50)
 
 font.init()
 font = font.Font(None, 35)
@@ -53,6 +55,33 @@ while game:
      for e in event.get():
        if e.type == QUIT:
            game = False
+     if finish != True:
+        window.fill(back)
+        racket1.update_r()
+        racket2.update_l()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racket1,ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+           speed_y *= -1
 
-display.update()
-clock.tick(FPS)
+       #if ball flies behind this paddle, display loss condition for player 1
+        if ball.rect.x < 0:
+           finish = True
+           window.blit(lose1, (200, 200))
+           game_over = True
+
+       #if the ball flies behind this paddle, display loss condition for player 2
+        if ball.rect.x > win_width:
+           finish = True
+           window.blit(lose2, (200, 200))        
+           game_over = True
+        
+     racket1.reset()
+     racket2.reset()
+     ball.reset()
+     display.update()
+     clock.tick(FPS)
+
